42341e8df6a553cd43aadc974a791b3141c2e117
Jieun Cha <chaji6516@naver.com>
2025-05-12 17:22:35 +0900

    sql 폴더 추가

---
+++ src/main/webapp/resources/sql/review_3.sql
@@ -0,0 +1,186 @@
+--------------------------------------------------------
+--  파일이 생성됨 - 월요일-5월-12-2025   
+--------------------------------------------------------
+--------------------------------------------------------
+--  DDL for Table REVIEW_3
+--------------------------------------------------------
+
+  CREATE TABLE "MBC"."REVIEW_3" 
+   (   "ID" VARCHAR2(20 BYTE), 
+   "STORECODE" NUMBER(7,0), 
+   "CONTENT" VARCHAR2(200 BYTE), 
+   "STARS" NUMBER(2,0), 
+   "TITLE" VARCHAR2(50 BYTE)
+   ) SEGMENT CREATION IMMEDIATE 
+  PCTFREE 10 PCTUSED 40 INITRANS 1 MAXTRANS 255 NOCOMPRESS LOGGING
+  STORAGE(INITIAL 65536 NEXT 1048576 MINEXTENTS 1 MAXEXTENTS 2147483645
+  PCTINCREASE 0 FREELISTS 1 FREELIST GROUPS 1 BUFFER_POOL DEFAULT FLASH_CACHE DEFAULT CELL_FLASH_CACHE DEFAULT)
+  TABLESPACE "SYSTEM" ;
+REM INSERTING into MBC.REVIEW_3
+SET DEFINE OFF;
+Insert into MBC.REVIEW_3 (ID,STORECODE,CONTENT,STARS,TITLE) values ('appple01',27,'전 맛있어요',5,'맛있던디');
+Insert into MBC.REVIEW_3 (ID,STORECODE,CONTENT,STARS,TITLE) values ('appple01',8,'맛있어요',5,'맛있어');
+Insert into MBC.REVIEW_3 (ID,STORECODE,CONTENT,STARS,TITLE) values ('appple01',30,'굿이다',4,'굿굿');
+Insert into MBC.REVIEW_3 (ID,STORECODE,CONTENT,STARS,TITLE) values ('appple01',25,'맛있어요',5,'잘 먹었슴돠');
+Insert into MBC.REVIEW_3 (ID,STORECODE,CONTENT,STARS,TITLE) values ('mitskgy123',27,'그냥 그런디',3,'그냥 그럼');
+Insert into MBC.REVIEW_3 (ID,STORECODE,CONTENT,STARS,TITLE) values ('appple01',23,'정말 맛있다',4,'예약해서 찾아갔어요');
+Insert into MBC.REVIEW_3 (ID,STORECODE,CONTENT,STARS,TITLE) values ('mmss',30,'보통이요',3,'보통입니다');
+Insert into MBC.REVIEW_3 (ID,STORECODE,CONTENT,STARS,TITLE) values ('mitskg1234',30,'되라됭하',3,'제발 되라');
+Insert into MBC.REVIEW_3 (ID,STORECODE,CONTENT,STARS,TITLE) values ('testuser',24,'존맛입니다
+',4,'JMT');
+Insert into MBC.REVIEW_3 (ID,STORECODE,CONTENT,STARS,TITLE) values ('testuser',21,'보통',3,'보통임');
+Insert into MBC.REVIEW_3 (ID,STORECODE,CONTENT,STARS,TITLE) values ('testuser',12,'맛나요를레이',5,'맛나');
+Insert into MBC.REVIEW_3 (ID,STORECODE,CONTENT,STARS,TITLE) values ('testuser',8,'짜장이다 짬뽕',3,'짜장');
+Insert into MBC.REVIEW_3 (ID,STORECODE,CONTENT,STARS,TITLE) values ('testuser',25,'굿입니다',4,'굿');
+Insert into MBC.REVIEW_3 (ID,STORECODE,CONTENT,STARS,TITLE) values ('testuser',14,'노맛',2,'맛도 제로');
+Insert into MBC.REVIEW_3 (ID,STORECODE,CONTENT,STARS,TITLE) values ('testuser',28,'굿굿',5,'초밥은 맛있다');
+Insert into MBC.REVIEW_3 (ID,STORECODE,CONTENT,STARS,TITLE) values ('testuser',13,'맛난데',5,'맛나요');
+Insert into MBC.REVIEW_3 (ID,STORECODE,CONTENT,STARS,TITLE) values ('testuser',2,'맛은 있음',4,'맛은 있지만 평범한 맛~');
+Insert into MBC.REVIEW_3 (ID,STORECODE,CONTENT,STARS,TITLE) values ('testuser',9,'굿구굿',4,'굿');
+Insert into MBC.REVIEW_3 (ID,STORECODE,CONTENT,STARS,TITLE) values ('testuser',7,'별로',1,'노노놉');
+Insert into MBC.REVIEW_3 (ID,STORECODE,CONTENT,STARS,TITLE) values ('testuser',11,'또 가야지',4,'헐');
+Insert into MBC.REVIEW_3 (ID,STORECODE,CONTENT,STARS,TITLE) values ('testuser',15,'노맛임',2,'노맛');
+Insert into MBC.REVIEW_3 (ID,STORECODE,CONTENT,STARS,TITLE) values ('testuser',27,'별로다 넘넘',1,'우우우ㅜㅇ');
+Insert into MBC.REVIEW_3 (ID,STORECODE,CONTENT,STARS,TITLE) values ('korea',7,'맛이 좋아요',4,'예약해서 갔는데 맛있 ');
+Insert into MBC.REVIEW_3 (ID,STORECODE,CONTENT,STARS,TITLE) values ('testuser',23,'맛나다',4,'꺄ㅑㅑㅑㅑ');
+Insert into MBC.REVIEW_3 (ID,STORECODE,CONTENT,STARS,TITLE) values ('testuser',22,'다시 올게요',4,'옿ㄹ');
+Insert into MBC.REVIEW_3 (ID,STORECODE,CONTENT,STARS,TITLE) values ('testuser',20,'넘 이상해',1,'맛이 왜 이래?');
+Insert into MBC.REVIEW_3 (ID,STORECODE,CONTENT,STARS,TITLE) values ('testuser',19,'보각보각',3,'보각');
+Insert into MBC.REVIEW_3 (ID,STORECODE,CONTENT,STARS,TITLE) values ('testuser',18,'옹오~~',4,'ㄱㄱ');
+Insert into MBC.REVIEW_3 (ID,STORECODE,CONTENT,STARS,TITLE) values ('testuser',17,'괜찮네여',4,'맛있');
+Insert into MBC.REVIEW_3 (ID,STORECODE,CONTENT,STARS,TITLE) values ('testuser',16,'맛있어',4,'맛있어요');
+Insert into MBC.REVIEW_3 (ID,STORECODE,CONTENT,STARS,TITLE) values ('testuser',10,'별로',2,'별로예요');
+Insert into MBC.REVIEW_3 (ID,STORECODE,CONTENT,STARS,TITLE) values ('testuser',6,'맛있',4,'맛있어요');
+Insert into MBC.REVIEW_3 (ID,STORECODE,CONTENT,STARS,TITLE) values ('testuser',5,'별로예요별로예요',1,'별로예요');
+Insert into MBC.REVIEW_3 (ID,STORECODE,CONTENT,STARS,TITLE) values ('testuser',4,'맛있어요맛있어요맛있어요',5,'맛있어요');
+Insert into MBC.REVIEW_3 (ID,STORECODE,CONTENT,STARS,TITLE) values ('testuser',3,'별로임',2,'별로임요');
+Insert into MBC.REVIEW_3 (ID,STORECODE,CONTENT,STARS,TITLE) values ('testuser',1,'맛있어요
+맛있어요
+',4,'맛있어요');
+Insert into MBC.REVIEW_3 (ID,STORECODE,CONTENT,STARS,TITLE) values ('appple01',28,'맛있다',5,'맛있');
+Insert into MBC.REVIEW_3 (ID,STORECODE,CONTENT,STARS,TITLE) values ('mitskgy123',28,'와옹웅웅!!',4,'와우');
+Insert into MBC.REVIEW_3 (ID,STORECODE,CONTENT,STARS,TITLE) values ('mitskgy123',4,'존맛탱구리',5,'존맛');
+Insert into MBC.REVIEW_3 (ID,STORECODE,CONTENT,STARS,TITLE) values ('korea',8,'굿',5,'불향');
+Insert into MBC.REVIEW_3 (ID,STORECODE,CONTENT,STARS,TITLE) values ('testuser',30,'맛있네요',3,'드디어 오넹');
+Insert into MBC.REVIEW_3 (ID,STORECODE,CONTENT,STARS,TITLE) values ('mitt12',30,'맛있어용~~~',3,'맛있다');
+Insert into MBC.REVIEW_3 (ID,STORECODE,CONTENT,STARS,TITLE) values ('korea',36,'정말 맛있어',5,'맛나');
+Insert into MBC.REVIEW_3 (ID,STORECODE,CONTENT,STARS,TITLE) values ('appple01',35,'맛이 끝내줌',4,'맛있어요');
+Insert into MBC.REVIEW_3 (ID,STORECODE,CONTENT,STARS,TITLE) values ('appple01',36,'그냥저냥해요',3,'그냥저냥');
+Insert into MBC.REVIEW_3 (ID,STORECODE,CONTENT,STARS,TITLE) values ('appple01',32,'와우',5,'정말 맛있엉');
+Insert into MBC.REVIEW_3 (ID,STORECODE,CONTENT,STARS,TITLE) values ('appple01',33,'jmtjmt
+',4,'와 jmt');
+Insert into MBC.REVIEW_3 (ID,STORECODE,CONTENT,STARS,TITLE) values ('appple01',31,'맛있~',5,'맛나다');
+Insert into MBC.REVIEW_3 (ID,STORECODE,CONTENT,STARS,TITLE) values ('appple01',29,'별론디요',2,'별로');
+Insert into MBC.REVIEW_3 (ID,STORECODE,CONTENT,STARS,TITLE) values ('appple01',26,'맛있어여여여여',4,'맛남');
+Insert into MBC.REVIEW_3 (ID,STORECODE,CONTENT,STARS,TITLE) values ('appple01',24,'아쉬워',2,'좀 아쉽');
+Insert into MBC.REVIEW_3 (ID,STORECODE,CONTENT,STARS,TITLE) values ('testuser',36,'정말 굿이예요',5,'구웃');
+Insert into MBC.REVIEW_3 (ID,STORECODE,CONTENT,STARS,TITLE) values ('korea',9,'아쉬워',3,'쫌 아쉽');
+Insert into MBC.REVIEW_3 (ID,STORECODE,CONTENT,STARS,TITLE) values ('korea',34,'맛있다',5,'맛있었어');
+Insert into MBC.REVIEW_3 (ID,STORECODE,CONTENT,STARS,TITLE) values ('korea',10,'고기 최고고기 최고고기 최고',5,'고기 최고');
+Insert into MBC.REVIEW_3 (ID,STORECODE,CONTENT,STARS,TITLE) values ('korea',32,'진짜 맛있네요',5,'와');
+Insert into MBC.REVIEW_3 (ID,STORECODE,CONTENT,STARS,TITLE) values ('korea',33,'굿굿',5,'굿입니다');
+Insert into MBC.REVIEW_3 (ID,STORECODE,CONTENT,STARS,TITLE) values ('korea',31,'별로예요',1,'별로');
+Insert into MBC.REVIEW_3 (ID,STORECODE,CONTENT,STARS,TITLE) values ('korea',30,'정말 좋네요',5,'하하');
+Insert into MBC.REVIEW_3 (ID,STORECODE,CONTENT,STARS,TITLE) values ('korea',29,'일식 잘하네',5,'일식');
+Insert into MBC.REVIEW_3 (ID,STORECODE,CONTENT,STARS,TITLE) values ('korea',27,'와우',5,'아아 개맛있어');
+Insert into MBC.REVIEW_3 (ID,STORECODE,CONTENT,STARS,TITLE) values ('korea',26,'친절하지 않아',1,'아쉬워요');
+Insert into MBC.REVIEW_3 (ID,STORECODE,CONTENT,STARS,TITLE) values ('korea',25,'굿굿굿굿',5,'굿굿');
+Insert into MBC.REVIEW_3 (ID,STORECODE,CONTENT,STARS,TITLE) values ('korea',24,'다 맛있네',4,'맛있어요');
+Insert into MBC.REVIEW_3 (ID,STORECODE,CONTENT,STARS,TITLE) values ('korea',23,'두번은 안 옴',3,'그냥한번정도올만함');
+Insert into MBC.REVIEW_3 (ID,STORECODE,CONTENT,STARS,TITLE) values ('korea',22,'별론디',1,'ㅋㅋㅋㅋㅋ');
+Insert into MBC.REVIEW_3 (ID,STORECODE,CONTENT,STARS,TITLE) values ('korea',21,'맛나요~~~~~~~~~',5,'맛있어요');
+Insert into MBC.REVIEW_3 (ID,STORECODE,CONTENT,STARS,TITLE) values ('korea',20,'그래도 꽤괜~',4,'괜찮은데 좀 아쉬워요');
+Insert into MBC.REVIEW_3 (ID,STORECODE,CONTENT,STARS,TITLE) values ('korea',19,'맛집이여',3,'맛집인듯');
+Insert into MBC.REVIEW_3 (ID,STORECODE,CONTENT,STARS,TITLE) values ('korea',18,'소수헌 숫수숫',3,'소수');
+Insert into MBC.REVIEW_3 (ID,STORECODE,CONTENT,STARS,TITLE) values ('korea',17,'맛있',4,'맛있어요');
+Insert into MBC.REVIEW_3 (ID,STORECODE,CONTENT,STARS,TITLE) values ('korea',16,'다시 올게요',5,'넘 만족');
+Insert into MBC.REVIEW_3 (ID,STORECODE,CONTENT,STARS,TITLE) values ('korea',15,'좋은 인테리어다',3,'굿 인테리어');
+Insert into MBC.REVIEW_3 (ID,STORECODE,CONTENT,STARS,TITLE) values ('korea',14,'맛있던데여',5,'맛나는디');
+Insert into MBC.REVIEW_3 (ID,STORECODE,CONTENT,STARS,TITLE) values ('korea',13,'정말 만족스러운 식사',5,'만족합니다');
+Insert into MBC.REVIEW_3 (ID,STORECODE,CONTENT,STARS,TITLE) values ('korea',12,'좋아',4,'좋아요');
+Insert into MBC.REVIEW_3 (ID,STORECODE,CONTENT,STARS,TITLE) values ('korea',11,'맛있네요',4,'굿굿');
+Insert into MBC.REVIEW_3 (ID,STORECODE,CONTENT,STARS,TITLE) values ('korea',6,'별로 또 안 감',1,'별로');
+Insert into MBC.REVIEW_3 (ID,STORECODE,CONTENT,STARS,TITLE) values ('korea',5,'뜨끈해',5,'뜨끈하다');
+Insert into MBC.REVIEW_3 (ID,STORECODE,CONTENT,STARS,TITLE) values ('korea',4,'맛이 넘 좋아',5,'쩌는맛');
+Insert into MBC.REVIEW_3 (ID,STORECODE,CONTENT,STARS,TITLE) values ('korea',3,'쌈밥은 항상 옳다',4,'쌈밥 최고');
+Insert into MBC.REVIEW_3 (ID,STORECODE,CONTENT,STARS,TITLE) values ('korea',2,'만두 ㄱㅊ음',3,'만두 좋다');
+Insert into MBC.REVIEW_3 (ID,STORECODE,CONTENT,STARS,TITLE) values ('korea',1,'맛있네',5,'올');
+Insert into MBC.REVIEW_3 (ID,STORECODE,CONTENT,STARS,TITLE) values ('mitskgy123',35,'맛있다맛있다맛있다',5,'맛있다');
+Insert into MBC.REVIEW_3 (ID,STORECODE,CONTENT,STARS,TITLE) values ('mitskgy123',26,'정말 노맛',2,'정말 노맛');
+Insert into MBC.REVIEW_3 (ID,STORECODE,CONTENT,STARS,TITLE) values ('mitskgy123',30,'굿굿굿굿굿굿굿굿',5,'굿굿굿');
+Insert into MBC.REVIEW_3 (ID,STORECODE,CONTENT,STARS,TITLE) values ('mitskgy123',32,'별로 다신 안옴',2,'별로');
+Insert into MBC.REVIEW_3 (ID,STORECODE,CONTENT,STARS,TITLE) values ('mitskgy123',31,'굿굿굿굿',4,'진짜 굿');
+Insert into MBC.REVIEW_3 (ID,STORECODE,CONTENT,STARS,TITLE) values ('mitskgy123',29,'별로다 정말',1,'별로 다신 안옴');
+Insert into MBC.REVIEW_3 (ID,STORECODE,CONTENT,STARS,TITLE) values ('mitskgy123',25,'진짜 굿인데',5,'진짜 굿');
+Insert into MBC.REVIEW_3 (ID,STORECODE,CONTENT,STARS,TITLE) values ('mitskgy123',24,'정말 맛있네',4,'정말 맛있다');
+Insert into MBC.REVIEW_3 (ID,STORECODE,CONTENT,STARS,TITLE) values ('mitskgy123',23,'다신 안옴',2,'별로');
+Insert into MBC.REVIEW_3 (ID,STORECODE,CONTENT,STARS,TITLE) values ('mitskgy123',22,'진짜 굿진짜 굿진짜 굿',3,'진짜 굿');
+Insert into MBC.REVIEW_3 (ID,STORECODE,CONTENT,STARS,TITLE) values ('mitskgy123',21,'노맛',2,'정말 노맛');
+Insert into MBC.REVIEW_3 (ID,STORECODE,CONTENT,STARS,TITLE) values ('mitskgy123',20,'맛있던데요??',5,'정말 맛있다');
+Insert into MBC.REVIEW_3 (ID,STORECODE,CONTENT,STARS,TITLE) values ('mitskgy123',19,'별로',1,'다신 안옴');
+Insert into MBC.REVIEW_3 (ID,STORECODE,CONTENT,STARS,TITLE) values ('mitskgy123',18,'굿입니다~',5,'굿');
+Insert into MBC.REVIEW_3 (ID,STORECODE,CONTENT,STARS,TITLE) values ('mitskgy123',17,'정말 노맛정말 노맛정말 노맛',3,'정말 노맛');
+Insert into MBC.REVIEW_3 (ID,STORECODE,CONTENT,STARS,TITLE) values ('mitskgy123',16,'굿굿굿굿굿굿굿',5,'굿');
+Insert into MBC.REVIEW_3 (ID,STORECODE,CONTENT,STARS,TITLE) values ('mitskgy123',15,'별로별로별로',2,'별로 다신 안옴');
+Insert into MBC.REVIEW_3 (ID,STORECODE,CONTENT,STARS,TITLE) values ('mitskgy123',13,'정말 맛있다',5,'정말 맛있다');
+Insert into MBC.REVIEW_3 (ID,STORECODE,CONTENT,STARS,TITLE) values ('mitskgy123',12,'다신 안옴',2,'정말 별로 ');
+Insert into MBC.REVIEW_3 (ID,STORECODE,CONTENT,STARS,TITLE) values ('mitskgy123',11,'진짜 굿이여',5,'진짜 굿');
+Insert into MBC.REVIEW_3 (ID,STORECODE,CONTENT,STARS,TITLE) values ('mitskgy123',10,'맛있어요~',4,'정말 맛있다');
+Insert into MBC.REVIEW_3 (ID,STORECODE,CONTENT,STARS,TITLE) values ('mitskgy123',9,'노맛임.',1,'노맛');
+Insert into MBC.REVIEW_3 (ID,STORECODE,CONTENT,STARS,TITLE) values ('mitskgy123',6,'베리굿',5,'굿');
+Insert into MBC.REVIEW_3 (ID,STORECODE,CONTENT,STARS,TITLE) values ('mitskgy123',8,'굿',5,'진짜 굿');
+Insert into MBC.REVIEW_3 (ID,STORECODE,CONTENT,STARS,TITLE) values ('mitskgy123',7,'정말 노맛',2,'정말 노맛');
+Insert into MBC.REVIEW_3 (ID,STORECODE,CONTENT,STARS,TITLE) values ('mitskgy123',5,'맛있다',3,'정말 맛있다');
+Insert into MBC.REVIEW_3 (ID,STORECODE,CONTENT,STARS,TITLE) values ('mitskgy123',3,'별로 다신 안옴별로 다신 안옴별로 다신 안옴',3,'별로 다신 안옴');
+Insert into MBC.REVIEW_3 (ID,STORECODE,CONTENT,STARS,TITLE) values ('mitskgy123',2,'진짜 굿',5,'정말 맛있다');
+Insert into MBC.REVIEW_3 (ID,STORECODE,CONTENT,STARS,TITLE) values ('mitskgy123',1,'정말 노맛',3,'별로 다신 안옴');
+Insert into MBC.REVIEW_3 (ID,STORECODE,CONTENT,STARS,TITLE) values ('mmss',36,'다신 안옴',1,'별로 다신 안옴');
+Insert into MBC.REVIEW_3 (ID,STORECODE,CONTENT,STARS,TITLE) values ('mmss',35,'진짜 굿임돠',5,'진짜 굿');
+Insert into MBC.REVIEW_3 (ID,STORECODE,CONTENT,STARS,TITLE) values ('mmss',34,'별로',2,'오올');
+Insert into MBC.REVIEW_3 (ID,STORECODE,CONTENT,STARS,TITLE) values ('mmss',33,'정말 맛있다',5,'진짜 굿');
+Insert into MBC.REVIEW_3 (ID,STORECODE,CONTENT,STARS,TITLE) values ('mmss',32,'노맛',1,'정말 노맛');
+Insert into MBC.REVIEW_3 (ID,STORECODE,CONTENT,STARS,TITLE) values ('mmss',31,'진짜 굿입니다',3,'진짜 굿');
+Insert into MBC.REVIEW_3 (ID,STORECODE,CONTENT,STARS,TITLE) values ('mmss',29,'진짜 굿',5,'정말 맛있다');
+Insert into MBC.REVIEW_3 (ID,STORECODE,CONTENT,STARS,TITLE) values ('mmss',28,'정말 노맛',1,'별로 다신 안옴');
+Insert into MBC.REVIEW_3 (ID,STORECODE,CONTENT,STARS,TITLE) values ('mmss',27,'굿',5,'진짜 굿');
+Insert into MBC.REVIEW_3 (ID,STORECODE,CONTENT,STARS,TITLE) values ('mmss',26,'다신 안옴
+',1,'별로 다신 안옴');
+Insert into MBC.REVIEW_3 (ID,STORECODE,CONTENT,STARS,TITLE) values ('mmss',25,'굿이여~~~~~~~~',5,'굿');
+Insert into MBC.REVIEW_3 (ID,STORECODE,CONTENT,STARS,TITLE) values ('mmss',24,'노맛',2,'정말 노맛');
+Insert into MBC.REVIEW_3 (ID,STORECODE,CONTENT,STARS,TITLE) values ('mmss',23,'진짜 굿',5,'정말 맛있다');
+Insert into MBC.REVIEW_3 (ID,STORECODE,CONTENT,STARS,TITLE) values ('mmss',22,'노맛이여',2,'노맛');
+Insert into MBC.REVIEW_3 (ID,STORECODE,CONTENT,STARS,TITLE) values ('mmss',21,'진짜 굿진짜 굿진짜 굿',5,'진짜 굿');
+Insert into MBC.REVIEW_3 (ID,STORECODE,CONTENT,STARS,TITLE) values ('mmss',20,'다신 안옴
+',1,'별로 다신 안옴');
+Insert into MBC.REVIEW_3 (ID,STORECODE,CONTENT,STARS,TITLE) values ('mmss',19,'굿굿굿굿굿',5,'굿');
+Insert into MBC.REVIEW_3 (ID,STORECODE,CONTENT,STARS,TITLE) values ('siu123',18,'노맛',2,'정말 노맛');
+Insert into MBC.REVIEW_3 (ID,STORECODE,CONTENT,STARS,TITLE) values ('siu123',17,'맛있다',5,'정말 맛있다');
+Insert into MBC.REVIEW_3 (ID,STORECODE,CONTENT,STARS,TITLE) values ('siu123',16,'별로',1,'다신 안옴');
+Insert into MBC.REVIEW_3 (ID,STORECODE,CONTENT,STARS,TITLE) values ('siu123',15,'진짜 굿진짜 굿진짜 굿진짜 굿',3,'진짜 굿');
+Insert into MBC.REVIEW_3 (ID,STORECODE,CONTENT,STARS,TITLE) values ('siu123',14,'정말 맛있다
+',5,'맛있다');
+Insert into MBC.REVIEW_3 (ID,STORECODE,CONTENT,STARS,TITLE) values ('siu123',13,'다시는 안옴',2,'별로 다신 안옴');
+Insert into MBC.REVIEW_3 (ID,STORECODE,CONTENT,STARS,TITLE) values ('siu123',12,'진짜 굿진짜 굿진짜 굿',5,'진짜 굿');
+Insert into MBC.REVIEW_3 (ID,STORECODE,CONTENT,STARS,TITLE) values ('siu123',11,'노맛...',2,'노맛');
+Insert into MBC.REVIEW_3 (ID,STORECODE,CONTENT,STARS,TITLE) values ('siu123',10,'평타인 맛',3,'중간');
+Insert into MBC.REVIEW_3 (ID,STORECODE,CONTENT,STARS,TITLE) values ('siu123',9,'넘 맛있어요~~',5,'맛있다');
+Insert into MBC.REVIEW_3 (ID,STORECODE,CONTENT,STARS,TITLE) values ('siu123',8,'다신 안옴',2,'별로 ');
+Insert into MBC.REVIEW_3 (ID,STORECODE,CONTENT,STARS,TITLE) values ('siu123',7,'굿굿굿굿굿굿굿',5,'진짜 굿');
+Insert into MBC.REVIEW_3 (ID,STORECODE,CONTENT,STARS,TITLE) values ('siu123',6,'정말 노맛',1,'정말 노맛');
+Insert into MBC.REVIEW_3 (ID,STORECODE,CONTENT,STARS,TITLE) values ('siu123',5,'진짜 굿',5,'정말 맛있다');
+Insert into MBC.REVIEW_3 (ID,STORECODE,CONTENT,STARS,TITLE) values ('siu123',4,'다신 안옴!!!!!',1,'별로 다신 안옴');
+Insert into MBC.REVIEW_3 (ID,STORECODE,CONTENT,STARS,TITLE) values ('siu123',3,'굿굿굿굿',5,'진짜 굿');
+Insert into MBC.REVIEW_3 (ID,STORECODE,CONTENT,STARS,TITLE) values ('siu123',2,'노맛',2,'정말 노맛');
+Insert into MBC.REVIEW_3 (ID,STORECODE,CONTENT,STARS,TITLE) values ('siu123',1,'노맛노맛노맛노맛',2,'노맛');
+--------------------------------------------------------
+--  Constraints for Table REVIEW_3
+--------------------------------------------------------
+
+  ALTER TABLE "MBC"."REVIEW_3" MODIFY ("STORECODE" NOT NULL ENABLE);
+  ALTER TABLE "MBC"."REVIEW_3" MODIFY ("ID" NOT NULL ENABLE);
+--------------------------------------------------------
+--  Ref Constraints for Table REVIEW_3
+--------------------------------------------------------
+
+  ALTER TABLE "MBC"."REVIEW_3" ADD CONSTRAINT "FK_MEMBER_3_TO_REVIEW_3_1" FOREIGN KEY ("ID")
+     REFERENCES "MBC"."MEMBER_3" ("ID") ENABLE;
